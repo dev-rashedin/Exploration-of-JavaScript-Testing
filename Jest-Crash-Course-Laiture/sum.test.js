60f7829d92235572cacf9da5b1debda1576d99ef
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-02 17:33:31 +0600

    updated

--- Jest-Crash-Course-Laiture/sum.test.js
+++ Jest-Crash-Course-Laiture/sum.test.js
@@ -1,16 +1,16 @@
 const sum = require('./sum');
 
-test('add 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3)
+// })
+
+// it('should add 1 + 2 to equal 3', () => {
+//    expect(sum(1, 2)).toBe(3)
+// })
+
+describe('sum()', () => {
+  it('should add 1 + 2 to equal 3', () => { 
+    expect(sum(1, 2)).toBe(3)
+  })
 })
 
-test('two plus two is four', () => {
-  expect(2 + 2).toBe(4);
-});
-
-test('object assignment', () => {
-  const data = { one: 1 };
-  data['two'] = 2;
-
-  expect(data).toEqual({ one: 1, two: 2 });
-})
