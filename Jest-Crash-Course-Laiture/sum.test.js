ec3627e1dd385cd853366c9043fc262184ea0df0
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-04 13:43:29 +0600

    some more mathers explored

--- Jest-Crash-Course-Laiture/sum.test.js
+++ Jest-Crash-Course-Laiture/sum.test.js
@@ -12,5 +12,29 @@ describe('sum()', () => {
   it('should add 1 + 2 to equal 3', () => { 
     expect(sum(1, 2)).toBe(3)
   })
+
+  it('object assignment', () => {
+    const obj = {};
+    expect(obj).toEqual({})
+  })
+
+
+
 })
+
+// truthy or falsy
+describe('truthy or falsy', () => {
+  it('null', () => {
+    const n = null;
+
+    // expect(n).toBeTruthy()
+    expect(n).toBeFalsy()
+    expect(n).not.toBeTruthy()
+    expect(n).toBeNull()
+    // expect(n).toBeUndefined()
+    expect(n).not.toBeUndefined()
+  })
+  })
+
+
 
